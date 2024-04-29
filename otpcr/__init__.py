57a08a566a70587d94f06f0413719586b7210796
Bart Thate <bthate@dds.nl>
2024-04-29 21:19:23 +0200

    update description

--- otpcr/__init__.py
+++ otpcr/__init__.py
@@ -1,4 +1,4 @@
 # This file is placed in the Public Domain.
 
 
-"init"
+"OTP-CR-117/19 - Communication Record (CR) 117 of the year 2019 (117/19) to the Office of the Prosecutor (OTP) of the International Criminal Court (ICC)"
